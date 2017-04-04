f545978d970ee724a82b664df8e157f98ee08ec2
misha-pelykh <mpel9797dev@gmail.com>
2017-04-04 13:18:20 +0300

    added database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,13 @@ lock "3.8.0"
 branch = ENV['BRANCH'] || 'master'
 
 set :application, "ActiveMile"
-set :repo_url, "git@example.com:me/my_repo.git"
 set :repo_url, "git@github.com:milestep/ActiveMile.git"
 set :branch, branch
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/activemile"
+set :deploy_to, "/home/midnight/apps"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh

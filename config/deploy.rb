5620803d6a7e48bf50405a1b9402e925babb5585
misha-pelykh <mpel9797dev@gmail.com>
2017-04-05 14:04:09 +0300

    added secrets.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "activemile"
 set :repo_url, "git@github.com:milestep/ActiveMile.git"
 set :passenger_restart_with_touch, true
 set :deploy_to, "/home/midnight/apps/activemile"
-set :linked_files, %w(config/database.yml config/application.yml)
+set :linked_files, %w(config/database.yml config/application.yml config/secrets.yml)
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system node_modules)
 
 # Default branch is :master

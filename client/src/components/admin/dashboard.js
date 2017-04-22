58274a47090a4abb6d1dc2eb56e31f56144042d8
misha-pelykh <mpel9797dev@gmail.com>
2017-04-22 10:54:04 +0300

    protect workspaces from unauthorized users

--- client/src/components/admin/dashboard.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Dashboard extends Component {
-  render() {
-    return(
-      <div><h3>Admin Dashboard</h3></div>
-    );
-  }
-}
--- client/src/containers/root.js
+++ client/src/containers/root.js
@@ -1,31 +1,28 @@
 import React, { 
   Component, 
-  PropTypes }           from 'react';
-import { Provider }     from 'react-redux';
+  PropTypes }       from 'react';
+import { Provider } from 'react-redux';
 import { 
   Router, 
   Route, 
-  IndexRedirect, 
-  browserHistory }      from 'react-router';
-import RequireAuth      from '../containers/requireAuth';
-import App              from '../components/app';
-import NotFound         from '../components/errors/err404';
-import Workspaces       from '../components/workspaces/workspaces';
-import Dashboard        from '../components/admin/dashboard';
-import Login            from '../components/auth/login';
+  IndexRedirect }   from 'react-router';
+import RequireAuth  from '../containers/requireAuth';
+import App          from '../components/app';
+import Workspaces   from '../components/workspaces/workspaces';
+import Login        from '../components/auth/login';
+import NotFound     from '../components/errors/err404';
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRedirect to="workspaces" />
-    <Route path="workspaces" component={Workspaces} />
-
+    
     <Route component={RequireAuth(false)}>
       <Route path="login" component={Login} />
     </Route>
 
-    <Route path="admin" component={RequireAuth()}>
-      <IndexRedirect to="dashboard" />
-      <Route path="dashboard" component={Dashboard} />
+    <Route component={RequireAuth()}>
+      <IndexRedirect to="workspaces" />
+      <Route path="workspaces" component={Workspaces} />
     </Route>
 
     <Route path='*' component={NotFound} />

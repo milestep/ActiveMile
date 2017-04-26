fc464b0a44780ad09121c47a6d264c9150073c1c
misha-pelykh <mpel9797dev@gmail.com>
2017-04-26 21:50:03 +0300

    remove workspace_id param from articles serializer and controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -28,6 +28,6 @@ class Api::V1::ArticlesController < Api::V1::BaseController
   private
 
   def article_params
-    params.require(:article).permit(:title, :type, :workspace_id)
+    params.require(:article).permit(:title, :type)
   end
 end

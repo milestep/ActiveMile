e0b43526a51bc986a81f0dec4d94d4c35beb25a4
misha-pelykh <mpel9797dev@gmail.com>
2017-05-10 12:52:45 +0300

    minor fix

--- client/src/components/counterparties/list.js
+++ client/src/components/counterparties/list.js
@@ -28,39 +28,37 @@ export default class List extends Component {
         const isEdited = editedCounterparty === item.id ? true : false;
 
         return (
-          <div key={i}>
-            <li className="list-group-item">
-              { isEdited ?
-                <Edit
-                  handleUpdate={this.props.handleUpdate}
-                  types={this.props.types}
-                  counterparty={item}
-                  toggleEdited={toggleEdited}
-                />
-              :
-                <div className="counterparty-overlap">
-                  <div className="col-md-10">
-                    <div className="counterparty-overlap">
-                      <span className="col-md-6">{ item.name }</span>
-                      <span className="col-md-3">{ item.type }</span>
-                      <span className="col-md-3">{ item.date }</span>
-                    </div>
-                  </div>
-                  <div className="btn-group">
-                    <button
-                      className="btn btn-sm btn-primary"
-                      onClick={toggleEdited.bind(this, item.id, true)}
-                    >
-                      <i class="fa fa-pencil" aria-hidden="true"></i>
-                    </button>
-                    <button className="btn btn-sm btn-danger" onClick={handleDestroy.bind(this, item.id)}>
-                      <i className="fa fa-times" aria-hidden="true"></i>
-                    </button>
+          <li key={i} className="list-group-item">
+            { isEdited ?
+              <Edit
+                handleUpdate={this.props.handleUpdate}
+                types={this.props.types}
+                counterparty={item}
+                toggleEdited={toggleEdited}
+              />
+            :
+              <div className="counterparty-overlap">
+                <div className="col-md-10">
+                  <div className="counterparty-overlap">
+                    <span className="col-md-6">{ item.name }</span>
+                    <span className="col-md-3">{ item.type }</span>
+                    <span className="col-md-3">{ item.date }</span>
                   </div>
                 </div>
-              }
-            </li>
-          </div>
+                <div className="btn-group">
+                  <button
+                    className="btn btn-sm btn-primary"
+                    onClick={toggleEdited.bind(this, item.id, true)}
+                  >
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                  </button>
+                  <button className="btn btn-sm btn-danger" onClick={handleDestroy.bind(this, item.id)}>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                  </button>
+                </div>
+              </div>
+            }
+          </li>
         );
       });
     } else {

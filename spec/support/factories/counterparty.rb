9dd34584d236b4f12efd40ae2175681d068a70b9
misha-pelykh <mpel9797dev@gmail.com>
2017-07-13 22:28:11 +0300

    fixed specs

--- spec/support/factories/counterparty.rb
+++ spec/support/factories/counterparty.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :counterparty do
-    name          { Faker::App.name }
-    date          { Faker::Date.between(2.days.ago, Date.today) }
-    type          { Faker::App.name }
-    workspace     factory: :workspace
+    name       { Faker::App.name }
+    date       { Faker::Date.between(2.days.ago, Date.today) }
+    type       { ['Client', 'Vendor' , 'Other'].sample }
+    workspace  factory: :workspace
   end
 end

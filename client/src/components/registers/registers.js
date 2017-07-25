0d637c78313ce940b82559995e810869dfc6d8ad
misha-pelykh <mpel9797dev@gmail.com>
2017-07-25 19:55:35 +0300

    fixed one more bug

--- client/src/components/registers/registers.js
+++ client/src/components/registers/registers.js
@@ -6,6 +6,7 @@ import moment                               from 'moment';
 import { toaster }                          from '../../actions/alerts';
 import { actions as registerActions }       from '../../resources/registers';
 import { actions as subscriptionActions }   from '../../actions/subscriptions';
+import { actions as workspaceActions }      from '../../actions/workspaces'
 import RegisterForm                         from './form';
 import RegistersList                        from './list';
 import RegistersFilter                      from './filter';
@@ -18,6 +19,7 @@ const monthsNames = moment.monthsShort();
     registers: state.registers.items,
     articles: state.articles.items,
     counterparties: state.counterparties.items,
+    nextWorkspace: state.workspaces.app.next,
     isCreating: state.registers.isCreating,
     isResolved: {
       registers: state.subscriptions.registers.resolved,
@@ -29,6 +31,7 @@ const monthsNames = moment.monthsShort();
     actions: bindActionCreators({
       ...registerActions,
       ...subscriptionActions,
+      ...workspaceActions,
       toaster
     }, dispatch)
   })
@@ -39,6 +42,7 @@ export default class Registers extends Component {
     registers: PropTypes.array.isRequired,
     articles: PropTypes.array.isRequired,
     counterparties: PropTypes.array.isRequired,
+    nextWorkspace: PropTypes.object.isRequired,
     isResolved: PropTypes.object.isRequired,
     isCreating: PropTypes.bool
   };
@@ -74,11 +78,15 @@ export default class Registers extends Component {
   }
 
   componentWillReceiveProps(newProps) {
-    const isDataReady = this.isModelsFetched(this.subscriptions, newProps);
+    const isDataReady = this.isModelsFetched(this.subscriptions, newProps)
 
-    if (isDataReady) {
-      this.createRegistersState(newProps);
+    if (isDataReady || this.isNextWorkspaceChanged()) {
+      this.createRegistersState(newProps)
     }
+  }
+
+  isNextWorkspaceChanged() {
+    return this.props.actions.isNextWorkspaceChanged(this.props.nextWorkspace.id)
   }
 
   createRegistersState(props = false) {
@@ -86,20 +94,15 @@ export default class Registers extends Component {
 
     let filter = Object.assign({}, this.state.filter),
         current = Object.assign({}, this.state.current),
-        currentMonth,
+        currentMonth = current.year ? current.month : monthsNames[new Date().getMonth()],
         registers = [];
-
-    if (current.year === null)
-      currentMonth = new Date().getMonth()
-    else
-      currentMonth = new Date(Date.parse(`${current.month} ${current.year}`)).getMonth()
 
     props.registers.forEach((register, i) => {
       const date = new Date(register.date);
       const year = date.getFullYear();
       const month = date.getMonth();
 
-      if (month == currentMonth)
+      if (month == monthsNames.indexOf(currentMonth))
         registers.push(register);
 
       if (!filter.years.includes(year))
@@ -122,10 +125,10 @@ export default class Registers extends Component {
       }
     }
 
-    if (current.year === null) {
+    if (!current.year) {
       Object.assign(current, {
         year: filter.years[0],
-        month: monthsNames[currentMonth]
+        month: currentMonth
       });
     } else {
       Object.assign(current, {

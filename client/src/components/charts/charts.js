f052cc54a2d607c2d62ead2bfebfc1fdd79c32ae
VadymBoguslavsky <axixe92@gmail.com>
2017-08-02 16:57:43 +0300

    recharts

--- client/src/components/charts/charts.js
+++ client/src/components/charts/charts.js
@@ -7,8 +7,12 @@ import { actions as counterpartyActions } from '../../resources/counterparties';
 import { actions as subscriptionActions } from '../../actions/subscriptions';
 import { actions as workspaceActions }    from '../../actions/workspaces'
 import { toaster }                        from '../../actions/alerts';
-import {Bar as LineChart} from 'react-chartjs';
+import moment                             from 'moment';
 import { setStatePromise, pushUnique }    from '../../utils'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+const monthsNames = moment.monthsShort();
+
 @connect(
   state => ({
     articles: state.articles.items,
@@ -26,56 +30,32 @@ import { setStatePromise, pushUnique }    from '../../utils'
     }, dispatch)
   })
 )
-export default class Charts extends Component {
 
+export default class Charts extends Component {
   constructor(props) {
     super(props);
     this.types = ['Revenue', 'Cost'],
     this.toaster = props.actions.toaster();
     this.subscriptions = ['articles', 'registers'],
-    this.state = this.createInitialState(),
-    this.state = {
-      chartData:{
-      labels: ["January", "February", "March",
-               "April", "May", "June",
-               "July", "August", "September",
-               "October", "November", "December"],
-      datasets: [
-         {
-            label: "revenue",
-            fillColor: "#32CD32",
-            data: [3,7,4]
-        },
-        {
-            label: "cost",
-            fillColor: '#F62817',
-            data: [4,3,5]
-        },
-        {
-            label: "profit",
-            fillColor: "#008080",
-            data: [4,3,5]
-        }
-      ]
-    }
+    this.state = this.createInitialState()
   }
-}
 
-componentWillMount() {
+  componentWillMount() {
     this.props.actions.subscribe(this.subscriptions)
       .then(() => {
         if (this.props.registers.length === 0) {
           this.toaster.warning('There is no data for reports')
         }
-        console.log(this.props)
         this.createReportState()
       })
   }
+
   componentWillReceiveProps() {
     if (this.isNextWorkspaceChanged()) {
       this.createReportState()
     }
   }
+
   componentWillUnmount() {
     this.props.actions.unsubscribe(this.subscriptions)
   }
@@ -85,80 +65,67 @@ componentWillMount() {
   }
 
   createInitialState() {
-    const date = new Date(),
-          year = date.getFullYear()
+    let chartsData = []
+
+    for (var i = monthsNames.length - 1; i >= 0; i--) {
+      chartsData.unshift(
+        {
+          name: monthsNames[i],
+          Revenue: 0,
+          Cost: 0,
+          Profit: 0
+        }
+      );
+    }
 
     return {
-      isError: false,
-      isStateReady: false,
-      current: { year },
-      report: {
-        Revenue: [],
-        Cost: []
-      },
-      available: {
-        years: [ year ],
-      },
-      profit: {
-        common: 0,
-        Revenue: 0,
-        Cost: 0
-      },
-      collapsedArticles: {
-        Revenue: [],
-        Cost: []
-      },
+      currentYear: 2017,
+      chartsData: chartsData
     }
   }
+
   createReportState() {
-    const fakeState = this.createInitialState()
     const { registers, articles } = this.props
-    const { current } = this.state
+    const { currentYear } = this.state
+    let chartsData = Object.assign([], this.state.chartsData);
 
-    let { report, profit } = fakeState
-    let { years } = fakeState.available
+    for (var i = registers.length - 1; i >= 0; i--) {
+      let register = registers[i]
+      let dataNow = new Date(register.date)
 
-    registers.forEach(register => {
-      const registerDate = new Date(register.date),
-            registerYear = registerDate.getFullYear()
+      if (currentYear === dataNow.getFullYear()) {
+        const article = articles.find(article => article.id === register.article_id)
+        let modelTypeArticle = article.type
+        let modelMoun = monthsNames[dataNow.getMonth()]
 
-      const article = Object.assign({}, articles.find(article => article.id === register.article_id))
-      const reportType = report[article.type]
-
-      let reportArticle = reportType.length ?
-          reportType.find(article => article.id === register.article_id) : null
-
-      profit.common += this.getRegisterValue(article.type, register.value)
-      profit[article.type] += register.value
-
-      if (reportArticle) {
-        Object.assign(
-          reportArticle,
-          this.getArticleCounterparties(register, reportArticle)
-        )
-      } else {
-        reportType.push({
-          ...article,
-          ...this.getEmptyArticleCounterparties(register, article)
-        })
+        for (var j = chartsData.length - 1; j >= 0; j--) {
+          if (chartsData[j].name === modelMoun) {
+            chartsData[j][modelTypeArticle] += register.value
+            chartsData[j].Profit = chartsData[j].Revenue - chartsData[j].Cost
+          }
+        }
       }
-    })
+    }
 
-    this.setState((prevState) => ({
-      ...prevState,
-      report,
-      profit,
-      isStateReady: true,
-      available: {
-        years: years.sort((a, b) => b - a)
-      }
-    }))
+    this.setState({
+      chartsData: chartsData
+    });
   }
+
   render() {
     return(
       <div>
         <h3>Charts</h3>
-          <LineChart data={this.state.chartData} options={null}  width="900" height="250"/>
+          <BarChart width={900} height={450} data={this.state.chartsData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Legend />
+            <Bar dataKey="Revenue" fill="#8884d8" />
+            <Bar dataKey="Cost" fill="#82ca9d" />
+            <Bar dataKey="Profit" fill="Orange" />
+          </BarChart>
       </div>
     );
   };

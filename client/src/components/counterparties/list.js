550c9d517c1f5ed1bc70da23531b109d675777c7
Oleksandr <targonsky97@gmail.com>
2017-11-02 13:07:03 +0200

    Remove registers_count from counterparty item

--- client/src/components/counterparties/list.js
+++ client/src/components/counterparties/list.js
@@ -48,9 +48,8 @@ export default class List extends Component {
             :
               <div className="tabs-overlap">
                 <div className="col-md-10">
-                  <span className={`col-md-9 ${ active ? '' : 'not-active' }`}>{ item.name }</span>
+                  <span className={`col-md-7 ${ active ? '' : 'not-active' }`}>{ item.name }</span>
                   <span className={`col-md-3 ${ active ? '' : 'not-active' }`}>{ moment(item.date).format("DD-MM-YYYY") }</span>
-                  <span className={`col-md-1 ${ active ? '' : 'not-active' }`}>{ item.registers_count }</span>
                 </div>
                 <div className="btn-group">
                   <button

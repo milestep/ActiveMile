9e544b9a4c3bd67ed1858c5f145ad0a46829925e
Oleksandr <targonsky97@gmail.com>
2017-11-08 09:44:20 +0200

    add before_destroy callback for Counterparty

--- app/models/counterparty.rb
+++ app/models/counterparty.rb
@@ -6,4 +6,13 @@ class Counterparty < ApplicationRecord
 
   validates :name, :date, :type, :workspace_id, presence: true
   validates :type, acceptance: { accept: ['Client', 'Vendor' , 'Other'] }
+
+  before_destroy :check_quantity_of_registers
+
+  private
+  def check_quantity_of_registers
+    if self.registers_count > 0 
+      throw :abort
+    end
+  end
 end

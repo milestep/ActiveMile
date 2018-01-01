bbeaf7e86fbc0692d9a08ffe151fb7cb4a6ff3b5
misha-pelykh <mpel9797dev@gmail.com>
2018-01-01 14:01:01 +0200

    update filter strategy

--- client/src/actions/filter/strategy.js
+++ client/src/actions/filter/strategy.js
@@ -1,12 +1,45 @@
 export default class FilterStrategy {
+  getFilters() {
+    return {
+      default: this.defaultFilters(),
+      component: this.componentFilters()
+    }
+  }
+
   createComponentFilter(value, name = value, applied = false) {
     return { value, name, applied }
+  }
+
+  checkComponentFilters(filters) {
+    if (!filters || filters.constructor != Object) {
+      throw new Error("'componentFilters' method must return an object")
+    }
+  }
+
+  handleFilters(inputFilters = {}) {
+    var newFilters = {}
+    var filters = {}
+
+    if (filters = inputFilters.component) {
+      this.checkComponentFilters(filters)
+      newFilters['component'] = filters
+    }
+
+    if (filters = inputFilters.default) {
+      newFilters['default'] = filters || null
+    }
+
+    return newFilters
   }
 
   /*
    * Abstract methods
    */
-  defaultFilters() { return null }
+  defaultFilters() {
+    return null
+  }
 
-  componentFilters() { return null }
+  componentFilters() {
+    throw new Error("'componentFilters' method should be created")
+  }
 }

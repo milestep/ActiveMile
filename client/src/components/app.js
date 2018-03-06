e7e8195bb189f6c3192c3b459fc823cb9f94b813
Axife <33604654+Definence@users.noreply.github.com>
2018-03-06 17:26:37 +0200

    Update app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -65,7 +65,6 @@ export default class App extends Component {
   }
 
   render() {
-    console.log(this.props.children)
     const { actions, logout } = this.props;
     const { store, router } = this.context;
     const { dispatch } = store;

9197d98fa087cac57b5c553f12a1ad110df13a51
Axife <dp210592ybo@gmail.com>
2018-03-22 17:45:33 +0200

    Add client & manager options to register form

--- client/src/components/registers/form.js
+++ client/src/components/registers/form.js
@@ -34,7 +34,7 @@ export default class RegisterForm extends Component {
   createRegisterState(props) {
     if (!props) props = this.props;
 
-    const { register, articles, counterparties } = props;
+    const { register, articles, counterparties, client, sales } = props;
     let counterpartyId, counterpartyName;
 
     if (register && register.counterparty) {
@@ -42,12 +42,32 @@ export default class RegisterForm extends Component {
       counterpartyName = register.counterparty.name;
     }
 
-    let forDate, forCounterparty, forArticle
+    if (register && register.client) {
+      clientId = register.client.id;
+      clientName = register.client.name;
+    }
+
+    if (register && register.sales) {
+      salesId = register.sales.id;
+      salesName = register.sales.name;
+    }
+
+    let forDate, forCounterparty, forArticle, forClient, forSales;
 
     if (this.state === undefined) {
       forDate = moment()
 
       forCounterparty = {
+        value: null,
+        label: null
+      }
+
+      forClient = {
+        value: null,
+        label: null
+      }
+
+      forSales = {
         value: null,
         label: null
       }
@@ -64,6 +84,16 @@ export default class RegisterForm extends Component {
         label: this.state.register.article.value.label
       }
 
+      forClient = {
+        value: this.state.register.client.value.value,
+        label: this.state.register.client.value.label
+      }
+
+      forSales = {
+        value: this.state.register.sales.value.value,
+        label: this.state.register.sales.value.label
+      }
+
       forCounterparty = {
         value: this.state.register.counterparty.value.value,
         label: this.state.register.counterparty.value.label
@@ -74,10 +104,22 @@ export default class RegisterForm extends Component {
       date: moment(Date.parse(register.date)),
       value: register.value,
       note: register.note,
+
       article: {
         value: register.article.id,
         label: register.article.title
       },
+
+      client: {
+        value: clientId,
+        label: clientName
+      },
+
+      sales: {
+        value: salesId,
+        label: salesName
+      },
+
       counterparty: {
         value: counterpartyId,
         label: counterpartyName
@@ -87,7 +129,9 @@ export default class RegisterForm extends Component {
       value: '',
       note: '',
       article: forArticle,
-      counterparty: forCounterparty
+      counterparty: forCounterparty,
+      client: forClient,
+      sales: forSales
     }, next = {};
 
     for (let i in prev) {
@@ -140,7 +184,7 @@ export default class RegisterForm extends Component {
   }
 
   render() {
-    const { date, value, note, article, counterparty } = this.state.register;
+    const { date, value, note, article, counterparty, client, sales } = this.state.register;
     const { currentFeatures, isFetching, editing, articles, counterparties } = this.props;
     const buttonCaption = editing ? 'Update' : 'Create Register';
     let clientOptions = [];
@@ -208,15 +252,17 @@ export default class RegisterForm extends Component {
             <FormSelect
               title="Client"
               name="client"
-              isBlured={counterparty.blured}
+              value={client.value}
+              isBlured={client.blured}
               options={clientOptions}
               handleChange={this.handleChange}
             />
 
             <FormSelect
               title="Sales manager"
-              name="salesManager"
-              isBlured={counterparty.blured}
+              name="sales"
+              value={sales.value}
+              isBlured={sales.blured}
               options={salesOptions}
               handleChange={this.handleChange}
             />

3eb37d51ae84ec77efd827c9805ef7f57f917d15
Sany18 <parubok.sashko@gmail.com>
2019-01-17 14:22:33 +0200

    Created basic of reports_controller

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -6,23 +6,30 @@ class Api::V1::ReportsController < Api::V1::BaseController
 
     counterparties_by_month(months, year).each do |item|
 
-      report[item['type']] = report.keys.include?(item['type']) ? report[item['type']] + '1' : '0'
+      # report[item['type']] = report.keys.include?(item['type']) ? report[item['type']] + '1' : '0'
       # counterparties_serialized.each {|key, value| puts "#{key} is #{value}" }
 
       # report[month] = { users: counterparties_serialized, revenue: cur_revenue, cost: cur_cost, profit: cur_profit }
-      # report[item['date']] = { item: 'bla' }
-      # puts item
+
+
+
+      if report.keys.include?(item['type'])
+        report[item['type']][item['title']] = {}
+      else
+        report[item['type']] = Hash[item['title'], {}]
+      end
     end
 
-    puts report
-    # render_api(report, :ok)
+    report[:total], report[:profit], report[:avg] = 0, 0, 0
+    render_api(report, :ok)
   end
 
   private
 
   def counterparties_by_month(namber_of_month, year)
     ActiveRecord::Base.connection.execute("
-      SELECT registers.date, name, value, articles.type, title FROM registers 
+      SELECT registers.date, registers.value, counterparties.name, articles.type, articles.title
+      FROM registers 
       INNER JOIN counterparties ON counterparties.id = registers.counterparty_id
       INNER JOIN articles ON registers.article_id = articles.id
       WHERE registers.workspace_id = #{current_workspace.id}
